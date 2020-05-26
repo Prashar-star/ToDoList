c8606ad6e782045bdc9be148855bb9d0a42ea06f
Pankaj Prashar <60200280+Prashar-star@users.noreply.github.com>
2020-05-26 18:55:26 +1200

    Added checkbox functionality

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,7 +5,11 @@ function TodoItem(props) {
   return (
     // <Header />
     <div className="mylist">
-      <input type="checkbox" checked={props.item.status} />
+      <input
+        type="checkbox"
+        checked={props.item.status}
+        onChange={() => props.handleChange(props.item.id)}
+      />
       <p>{props.item.number}</p>
       <p>{props.item.task}</p>
     </div>
